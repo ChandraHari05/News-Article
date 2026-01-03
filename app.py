b496c1f6c1951744b105b19106da7fec71f16267
ChandraHari <146060208+ChandraHari05@users.noreply.github.com>
2026-01-03 15:41:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+import requests
+from config import NEW_API_KEY
+NEW_API_KEY="bdf1bc24a5534431a47384837ef16e90"
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    query=request.args.get('query','latest')
+    url=f"https://newsapi.org/v2/everything?q={query}&apiKey={NEW_API_KEY}"
+    response=requests.get(url)
+    news_data = response.json()
+    articles=news_data.get('articles',[])
+    filtered_articles=[article for article in articles if "yahoo" not in article ["source"]["name"] and 'removed' not in article ['title'].lower()]
+    return render_template("index.html",articles=filtered_articles,query=query)
+
+if __name__ == "__main__":
+    app.run(debug=True)
